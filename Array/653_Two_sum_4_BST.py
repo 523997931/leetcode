7ee83288fd03fc8fe17cb952a13e154b9bf08121
hanpeiqi <hanpeiqi@autohome.com>
2019-11-06 13:53:11 +0800

    修改653题解

--- Array/653_Two_sum_4_BST.py
+++ Array/653_Two_sum_4_BST.py
@@ -36,11 +36,39 @@ class TreeNode:
          self.right = None
 
 class Solution:
-    def findTarget(self, root, k):
-        big = root
-        small = root
-        while(small!=None and big!=None):
-            if((big.val+small.val)==k):
-                return True
-            elif((big.val+small.val)>k):
-                small
+    def findTarget(self, root, k):      #采用树的中序遍历，便利其中每一个数，并使用一个set进行记录,二叉搜索树不存在相同元素，因此遍历后便可得到结果，执行效果：时间104ms，空间16.4MB
+        s = set()                       #时间O(n),空间O(n)
+        global mark
+        mark=0
+        self.mid_scan(root,s,k)
+        if(mark==1):
+            return True
+        else:
+            return False
+    def mid_scan(self,root,s,k):
+        global mark
+        if root==None:
+            return
+        else:
+            self.mid_scan(root.left,s,k)
+            if((k-root.val) in s):
+                mark = 1
+            s.add(root.val)
+            self.mid_scan(root.right,s,k)
+if __name__=='__main__':
+    node = TreeNode(5)
+    node2 = TreeNode(3)
+    node3 = TreeNode(6)
+    node4 = TreeNode(2)
+    node5= TreeNode(4)
+    node6 = TreeNode(7)
+    node.left=node2
+    node.right=node3
+    node2.left=node4
+    node2.right=node5
+    node3.right=node6
+    c=Solution()
+    target = 28
+    x = c.findTarget(node,target)
+    print(x)
+
